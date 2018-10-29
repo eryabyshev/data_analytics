e71f5aff2653ffb008405475eb498ee11980137e
Evgeny Ryabyshew <ru.evgeny.dev@gmail.com>
2018-10-29 23:36:59 +0300

    add hw3

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -20,9 +20,22 @@ psql --host $APP_POSTGRES_HOST -U postgres -c "SELECT COUNT(*) as num_ratings FR
 
 psql --host $APP_POSTGRES_HOST  -U postgres -c \
     "DROP TABLE IF EXISTS top_rated_tags"
+
+
 echo "Create table top_rated_tags.sql"
-psql --host $APP_POSTGRES_HOST -U postgres -f /home/top_rated_tags.sql
+psql --host $APP_POSTGRES_HOST -U postgres -c "with top_rated as
+(
+      select movieId, avg
+    from (
+        select links.movieid as movieId, count(links.movieid) as counter, avg(ratings.rating) as avg
+        from links join ratings on ratings.movieid = links.movieid
+        group by links.movieid
+        having count(links.movieid) > 50
+        order by avg DESC
+        ) as temp
+    limit 150
+)select top_rated.movieId, tags INTO top_rated_tags from top_rated join keywords on top_rated.movieId = keywords.movieid"
 
 echo "Import table to /data/"
 psql --host $APP_POSTGRES_HOST -U postgres -c \
-    "\\copy (select * from top_rated_tags) to '/data/keywords.csv' with delimiter as E','"
+    "\\copy (select * from keywords) to '/data/keywords.tsv' with delimiter as E'\t'"
